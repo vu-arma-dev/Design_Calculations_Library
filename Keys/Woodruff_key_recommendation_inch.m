ea7dd93547a53dd4d82e4ec5e5f80b64bd7ef25b
Garrison Johnston <garrison@SEMEW-HJKFZQ1.ds.vanderbilt.edu>
2018-10-16 17:11:53 -0500

    corrected logic mistake

--- Keys/Woodruff_key_recommendation_inch.m
+++ Keys/Woodruff_key_recommendation_inch.m
@@ -1,5 +1,5 @@
 function [ key_number, width, length, height ] = Woodruff_key_recommendation_inch( shaft_diameter )
-%% WOODRUFF_KEY_RECOMMENDATION: Recommends an inch woodruff key for a given shaft diameter of range 7-16 in to 2.5 inches. Based on ANSI Standard B17.2
+%% WOODRUFF_KEY_RECOMMENDATION: Recommends an inch woodruff key for a given shaft diameter of range 7-16 in to 2.5 inches. Based on ANSI Standard B17.2.
 %% [1] J. Collins, H. Busby and G. Staab, Mechanical design of machine elements and machines, 2nd ed. Hoboken: John Wiley & Sons, 2010.
 %% Garrison Johnston 10/16/2018
 %% INPUTS:
@@ -11,35 +11,37 @@ function [ key_number, width, length, height ] = Woodruff_key_recommendation_inc
 % length: length of the key [in]
 % height: height of the key [in]
 %% Main
-if ((7/16)<= shaft_diameter <= 1/2)
+if (((7/16)<= shaft_diameter) && (shaft_diameter <= 1/2))
     key_number = 305;
     width = 3/32;
     length = 5/8;
     height = 0.25;
-elseif ((11/16)<= shaft_diameter <= 3/4)
+elseif (((11/16)<= shaft_diameter) && (shaft_diameter <= 3/4))
     key_number = 405;
     width = 1/8;
     length = 5/8;
     height = 0.25;
-elseif ((13/16)<= shaft_diameter <= 15/16)
+elseif (((13/16)<= shaft_diameter) && (shaft_diameter <= 15/16))
     key_number = 506;
     width = 5/32;
     length = 3/4;
     height = 0.312;
-elseif (1<= shaft_diameter <= (1+3/16))
+elseif ((1<= shaft_diameter <= (1+3/16)) && (shaft_diameter <= (1+3/16)))
     key_number = 608;
     width = 3/16;
     length = 1;
     height = 0.437;
-elseif ((1+1/4)<= shaft_diameter <= (1+3/4))
+elseif (((1+1/4)<= shaft_diameter) && (shaft_diameter <= (1+3/4)))
     key_number = 809;
     width = 1/4;
     length = 1+1/8;
     height = 0.484;
-elseif ((1+7/8)<= shaft_diameter <= (2+1/2))
+elseif (((1+7/8)<= shaft_diameter) && (shaft_diameter <= (2+1/2)))
     key_number = 1212;
     width = 3/8;
     length = 1+1/2;
     height = 0.641;
+else
+    disp('Shaft diameter out of the range.')
 end
-
+%% NOTE: It is still nessesary to do a stress analysis. This is only a first pass design step. 
