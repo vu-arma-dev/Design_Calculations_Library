0403c4452c70b3d7f6b5c44d42bc90509c48b810
Garrison Johnston <garrison@SEMEW-HJKFZQ1.ds.vanderbilt.edu>
2019-01-30 10:18:41 -0600

    Added the option of using eithewr Von Mises or Tresca failure theory to determine FOS against shear

--- Keys/key_stresses.m
+++ Keys/key_stresses.m
@@ -1,4 +1,4 @@
-function [ shear_stress, compressive_stress, principle_stress_1, FOS ] = key_stresses( shaft_diameter, torque, width, height, length, yield_stress )
+function [ shear_stress, compressive_stress, FOS_shear, FOS_compressive ] = key_stresses( shaft_diameter, torque, width, height, length, yield_stress, theory )
 %% KEY_STRESSES: this function calculates the stesses and factor of safety in a key 
 %% [1] J. Collins, H. Busby and G. Staab, Mechanical design of machine elements and machines, 2nd ed. Hoboken: John Wiley & Sons, 2010.
 % Chapter 8.8. Pages 367-368
@@ -9,26 +9,40 @@ function [ shear_stress, compressive_stress, principle_stress_1, FOS ] = key_str
 % height: heigth of the key [m or in]
 % length: length of the key [m or in]
 % yeild_stress: yeild stress of the key material [Pa or psi]
+% theory: Gives the option of using 'vonMises' or 'tresca' to
+%         determine the max allowable shear stress. Defaults to 'tresca'.
 %% OUTPUTS
 % shear_stress: average shear stress in the key [pa or psi]
 % compressive_stress: compressive bearing stress in the key [pa or psi]
-% principle_stress_1: first principle stress in the key [pa or psi]
-% FOS: Factor of safety against the first principle stress
+% FOS_shear: Factor of safety against shear stress using 'vonMises' or 
+%            'tresca' failure theory.
+% FOS_compressive: Factor of safety against compressive stress
 %% Assumptions:
 % bearing stress from forces on the top and bottom of the key is zero
-%% Garrison Johnston 10/16/2018
-%% Compressive force
-F = torque/(shaft_diameter/2); % Compressive force in the key
+%% Revision History:
+% 10/16/2018: File Created -- Garrison Johnston 
+% 1/30/2019: Now calculates shear FOS and compressive stress FOS --
+%            Garrison Johnston
+%% Set default value of theory to tresca
+if nargin < 7
+    theory = 'tresca';
+end
 %% Average Shear stress
 As = width*length; % Area of the shearing plane
 shear_stress = F/As;
+%% Shear stress FOS
+if strcmp('tresca',theory)
+    shear_yield = yield_stress/2;
+elseif strcmp('vonMises',theory)
+    shear_yield = yield_stress/sqrt(3);
+end
+FOS_shear = shear_yield/shear_stress;
+%% Compressive force
+F = torque/(shaft_diameter/2); % Compressive force in the key
 %% Compressive bearing stress
 Acomp = height*length; % Area of the bearing stress contact plane
 compressive_stress = F/Acomp;
-%% First Principle Stress
-% sigma1 = (Mohr's circle center) + (Mohr's circle radius) 
-principle_stress_1 = compressive_stress/2+sqrt((compressive_stress/2)^2+shear_stress^2);
 %% Factor of safety
-FOS = yield_stress/principle_stress_1;
+FOS_compressive = yield_stress/compressive_sress;
 end
     
